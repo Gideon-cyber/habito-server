d6fe207b58fadac60334137139fa7ff354bf52fb
Gideon-Cyber <gideonjohnson70@gmail.com>
2023-03-14 22:55:04 +0100

    complete server

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const nodemailer = require("./routes/nodemailer");
+
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+  res.send("This is a server built with Express by gideon_cyber");
+});
+
+app.post("/api/nodemailer", (req, res) => {
+  try {
+    const { yourNeed, liveInProperty, userInputs } = req.body;
+    nodemailer({ yourNeed, liveInProperty, userInputs });
+    res.status(200).json({ message: "You have successfully registered" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+
+
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log("Server running on port " + port);
+});
---
+++ routes/nodemailer.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cron = require("node-cron");
+require("dotenv").config();
+
+// test email message options
+const mailer = ({ yourNeed, liveInProperty, userInputs }) => {
+  const mailOptions = {
+    from: "gideonjohnson70@gmail.com",
+    to: userInputs[1],
+    subject: "New House buyer",
+    html: `
+    <div style="width: 100%; background-color: #fff; padding: 5rem 0">
+    <div style="max-width: 700px; margin: 0 auto; background-color: #fff; margin: 0 auto">
+    <div style="width: 100%; background-color: #000; padding: 20px 0">
+    <a href="${process.env.CLIENT_URL}" ><img src="https://antonik116.sg-host.com/wp-content/uploads/2023/03/Logo-White-1.png" style="width: 100%; height: 70px; object-fit: contain" /></a>
+
+
+    </div>
+    <div style="width: 100%; gap: 10px: padding: 30px 0; display: grid">
+    <h1 style="font-size: 1.15rem; margin: 15px 20px">Here are the new customer details</h1>
+
+    <div style="font-size: .8rem; margin: 0 30px"><p>your need: <b>${yourNeed}</b></p>
+    <p>live in property: <b>${liveInProperty}</b></p>
+    
+    <p>name: <b>${userInputs[0]}</b></p>
+    <p>email: <b>${userInputs[1]}</b></p>
+    <p>number: <b>${userInputs[2]}</b></p></div>
+    </div>
+    </div>
+    </div>`,
+  };
+
+  // var doc = new jsPDF();
+
+  // doc.text(`your need: ${yourNeed}`, 10, 10);
+  // doc.text(`live in property: ${liveInProperty}`, 10, 20);
+  // doc.text(`name: ${userInputs[0]}`, 10, 30);
+  // doc.text(`email: ${userInputs[1]}`, 10, 40);
+  // doc.text(`number: ${userInputs[2]}`, 10, 50);
+  // doc.out;
+  // doc.save("newReg.pdf");
+
+  // transport info for nodemailer
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "gideonjohnson70@gmail.com",
+      pass: "eibanvrjdpzbxtzv",
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.log("Error Occurs", err);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
+
+module.exports = mailer;
